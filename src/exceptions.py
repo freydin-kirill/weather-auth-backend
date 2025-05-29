778e3863385a2652e49a2c39ffa6a79c35f3bd0c
Kirill <freydin.kirill@gmail.com>
2025-05-29 11:31:06 +0300

    Added exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,42 @@
+from fastapi import HTTPException, status
+
+
+UserNotFoundException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="User not found",
+)
+
+UserInactiveException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Inactive user",
+)
+
+UserEmailOrPasswordException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid email or password",
+)
+
+UserAlreadyExistsException = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail="User already exists",
+)
+
+TokenInvalidException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid toker",
+)
+
+TokenExpiredException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Token expired",
+)
+
+TokenNotFoundException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Token not found",
+)
+
+ForbiddenException = HTTPException(
+    status_code=status.HTTP_403_FORBIDDEN,
+    detail="Not enough privileges",
+)
